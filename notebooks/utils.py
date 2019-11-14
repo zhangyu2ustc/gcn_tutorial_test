f6d20f1b29b4b8373d35ab8f5fed7eab3b4056e3
zhangyu2ustc <zhangyu2ustc@gmail.com>
2019-11-13 23:24:56 -0500

    resolve different version of spmm function

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -15,6 +15,29 @@ from sklearn import preprocessing
 
 import torch
 from torch.utils.data import Dataset, DataLoader
+from torch_scatter import scatter_add
+
+
+def spmm(index, value, m, matrix):
+    """Matrix product of sparse matrix with dense matrix.
+
+    Args:
+        index (:class:`LongTensor`): The index tensor of sparse matrix.
+        value (:class:`Tensor`): The value tensor of sparse matrix.
+        m (int): The first dimension of sparse matrix.
+        matrix (:class:`Tensor`): The dense matrix.
+
+    :rtype: :class:`Tensor`
+    """
+
+    row, col = index
+    matrix = matrix if matrix.dim() > 1 else matrix.unsqueeze(-1)
+
+    out = matrix[col]
+    out = out * value.unsqueeze(-1)
+    out = scatter_add(out, row, dim=0, dim_size=m)
+
+    return out
 
 def extract_event_data(events_all_subjects_file,Trial_Num=None):
     ### loading event designs
